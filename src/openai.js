2140b49792d7c527717563e57b1428f39c774f75
VitorBrazSilva <vitorbraz_@hotmail.com>
2025-06-14 14:29:31 -0300

    atualização nas mensagens

--- src/openai.js
+++ src/openai.js
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 export async function gerarMensagem(nome, descricao) {
   const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY }); // ✅ move para dentro
 
-  const prompt = `Crie uma mensagem de parabéns para ${nome}, que ${descricao}`;
+  const prompt = `Crie uma mensagem de parabéns para ${nome}, com seguintes descrições ${descricao}`;
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: prompt }],
